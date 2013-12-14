000117ec67ad4c68b4c927ff01633ccd740e0e34
mobyvb <mobyvb@gmail.com>
2013-12-14 15:19:09 -0500

    basic drifting mechanics

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,22 @@ var viewport, win, then, now, fps, ctx, width, height;
 var TWO_PI = 2*Math.PI;
 var viewportModifier = 1;
 
+var keysDown = {};
+var car = {
+  width:50,
+  height:100,
+  angle:0,
+  positionX:500,
+  positionY:500,
+  velocityX:0,
+  velocityY:0,
+  angularVelocity:0,
+  drag:0.99,
+  angularDrag:0.9,
+  power:1000,
+  turnSpeed:30
+};
+
 function setup() {
   setupRAF();
   viewport = document.getElementById('viewport');
@@ -31,11 +47,40 @@ function frame() {
 function update(dt) {
   fps = ~~(1000/dt);
   elapsedSeconds = dt/1000;
+
+  if(keysDown[38]) { // forward
+    car.velocityY -= Math.cos(car.angle)*car.power*elapsedSeconds;
+    car.velocityX += Math.sin(car.angle)*car.power*elapsedSeconds;
+  }
+  else if(keysDown[40]) { // backward
+    car.velocityY += Math.cos(car.angle)*car.power*elapsedSeconds;
+    car.velocityX -= Math.sin(car.angle)*car.power*elapsedSeconds;
+  }
+
+  if(keysDown[37] && !keysDown[39]) { // left
+    car.angularVelocity -= car.turnSpeed*elapsedSeconds;
+  }
+  else if(keysDown[39] && !keysDown[37]) { // right
+    car.angularVelocity += car.turnSpeed*elapsedSeconds;
+  }
+
+  car.positionX += car.velocityX*elapsedSeconds;
+  car.positionY += car.velocityY*elapsedSeconds;
+  car.velocityX *= car.drag;
+  car.velocityY *= car.drag;
+  car.angle += car.angularVelocity*elapsedSeconds;
+  car.angularVelocity *= car.angularDrag;
 }
 
 function draw() {
+  var vpm = viewportModifier;
+  ctx.clearRect(0, 0, width*vpm, height*vpm);
   ctx.fillStyle="#FF0000";
-  ctx.fillRect(0,0,width*viewportModifier,height*viewportModifier);
+  ctx.translate(car.positionX*vpm, car.positionY*vpm);
+  ctx.rotate(car.angle);
+  ctx.fillRect(-car.width/2*vpm, -car.height/2*vpm, car.width*vpm, car.height*vpm);
+  ctx.rotate(-car.angle);
+  ctx.translate(-car.positionX*vpm, -car.positionY*vpm);
 }
 
 var viewportWidth, viewportHeight;
@@ -71,5 +116,13 @@ var setupRAF = function() {
       clearTimeout(id);
   };
 }
+
+addEventListener("keydown", function (e) {
+  keysDown[e.keyCode] = true;
+}, false);
+
+addEventListener("keyup", function (e) {
+  delete keysDown[e.keyCode];
+}, false);
 
 setup();
